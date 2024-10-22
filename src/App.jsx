88ae6d7d98f660b2fcb679a93bb019748d3c02e3
Gaurav <gauravkumarop3@gmail.com>
2024-10-22 17:13:42 +0530

    first Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import About from './components/Webpages/About/About'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './components/Footer/Footer';
+import Main from './components/Main/Main';
+import Header from './components/Header/Header'
+function App() {
+  return (
+    
+      <>
+        <Header/>
+        <Main/>
+        <Footer/>
+        {/* <About/> */}
+      </>
+
+  
+    
+  );
+}
+
+export default App;
