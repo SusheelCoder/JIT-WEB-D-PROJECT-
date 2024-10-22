8af03b1e42ccdb373fd6eb79a63154157d506f15
Gaurav <gauravkumarop3@gmail.com>
2024-10-22 17:43:55 +0530

    adding vision and mission

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 import Header from './components/Header/Header'
+import MissionAndVision from './components/Webpages/Mission and Vision/MissionAndVision';
 function App() {
   return (
     
       <>
-        <Header/>
+        {/* <Header/>
         <Main/>
-        <Footer/>
+        <Footer/> */}
         {/* <About/> */}
+        <MissionAndVision/>
       </>
 
   
